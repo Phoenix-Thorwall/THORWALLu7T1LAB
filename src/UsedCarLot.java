e94e397834aa278741e7a6aeff907a715c92761e
Phoenix <phoenix.thorwall@gmail.com>
2023-01-31 22:42:23 -0500

    DONE

--- src/UsedCarLot.java
+++ src/UsedCarLot.java
@@ -1,2 +1,37 @@
+import java.util.ArrayList;
 public class UsedCarLot {
+
+    private ArrayList<Car> inventory;
+
+    public UsedCarLot()
+    {
+        inventory = new ArrayList<Car>();
+    }
+
+    public ArrayList<Car> getLot()
+    {
+        return inventory;
+    }
+
+    public void addCar(Car newbie)
+    {
+        inventory.add(newbie);
+    }
+
+    public boolean swap(int idx1, int idx2)
+    {
+        if (idx1 < inventory.size() && idx2 < inventory.size())
+        {
+            Car car1 = inventory.get(idx1);
+            Car car2 = inventory.get(idx2);
+            inventory.set(idx1, car2);
+            inventory.set(idx2, car1);
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
+    }
 }
